13d868020fb4a38b746f6abce8578e4f75478062
Malte Dostal <malte.dostal@gmail.com>
2021-08-12 11:56:52 +0200

    add favicons and show/hide context

--- js/sanj0.js
+++ js/sanj0.js
@@ -1,3 +1,33 @@
 const birthday = new Date(2004, 0, 13);
+const footer_text = '&copy; 2021 Malte Dostal. All rights reserved.<br>Fonts by fonts.google.com ("Righteous", "Raleway")';
+
+for (let f of document.getElementsByClassName("footer")) {
+    f.innerHTML = footer_text;
+}
+
+init_context_visibility();
 
 document.getElementById("age").innerHTML = Math.floor((Date.now() - birthday) / 1000 / 60 / 60 / 24 / 365.4);
+const divstart = document.getElementById("divstart");
+divstart.onmouseover = divstart.onmouseout = change_context_visibility;
+
+function change_context_visibility(event) {
+    let hide = event.type == "mouseout";
+    change_context_display_style(hide ? "none" : "inline-block");
+}
+
+function change_context_display_style(d) {
+    for (let c of document.getElementsByClassName("context")) {
+        c.style.display = d;
+    }
+}
+
+function init_context_visibility() {
+    change_context_display_style(isTouchDevice() ? "inline-block" : "none");
+}
+
+function isTouchDevice() {
+    return (('ontouchstart' in window) ||
+       (navigator.maxTouchPoints > 0) ||
+       (navigator.msMaxTouchPoints > 0));
+  }
