47a59e0ace22720bca18e02c50c794586944be62
Malte Dostal <malte.dostal@gmail.com>
2021-09-13 21:01:50 +0200

    automatic deploy commit: update navbar color

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,22 @@
 # and commits and pushed the changes using the last commit message from the source repo
 before=$PWD
 # get last commit message
+echo "fetching last commit message.."
 commit_message=`git log -1 --pretty=%B`
 repo="../sanj0.github.io/"
+echo "... done."
 
 # minify all sources into gh pages repo
+echo "minifying all resources and copying them to ${repo}"
 minify -s --recursive -o $repo .
+echo "... done."
 
 # add all and commit with aforementioned message
 # do git stuff with && in case of failure
+echo "commiting and pushing ${repo}"
 cd $repo && git add . && git commit -m "automatic deploy commit: ${commit_message}" && git push
+echo "... done."
 
 # cd back
 cd $before
+echo "... done deploying."
